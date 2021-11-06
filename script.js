7cfebdaeeed218a56e2889fa0a058c5dfdca2fa0
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-11-06 19:01:35 -0300

    Req 02 - 03 done

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const text = document.querySelector('#text-input');
-const memeText = document.querySelector('#meme-text');
+const inputText = document.querySelector('#text-input');
+const inputImg = document.querySelector('#meme-insert');
 
-text.addEventListener('input', () => {
-  console.log(memeText);
-  memeText.innerHTML = text.value;
+const memeText = document.querySelector('#meme-text');
+const memeImage = document.querySelector('#meme-image');
+
+inputText.addEventListener('input', () => {
+  memeText.innerHTML = inputText.value;
+});
+
+// source https://www.webtrickshome.com/forum/how-to-display-uploaded-image-in-html-using-javascript
+inputImg.addEventListener('input', ({ target }) => {
+  memeImage.src = URL.createObjectURL(target.files[0]);
 });
