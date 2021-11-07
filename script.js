885be663855ad13db3b9e440ee53a3e2982b703e
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-11-07 20:28:15 -0300

    css 2.0

--- script.js
+++ script.js
@@ -1,19 +1,16 @@
+const borderDefault = document.querySelector('#default');
+const btnDownload = document.querySelector('.download');
 const container = document.querySelector('#meme-image-container');
-const preMemes = document.querySelector('#meme-templates').childNodes;
-
-const inputImg = document.querySelector('#meme-insert');
-const inputText = document.querySelector('#text-input');
-
-const memeImage = document.querySelector('#meme-image');
-const memeText = document.querySelector('#meme-text');
-
 const earth = document.querySelector('#earth');
 const fire = document.querySelector('#fire');
-const water = document.querySelector('#water');
-
-let btnDownload = document.querySelector('.download');
+const inputImg = document.querySelector('#meme-insert');
+const inputText = document.querySelector('#text-input');
+const localText = document.querySelectorAll('input[name="localText"]');
+const memeImage = document.querySelector('#meme-image');
+const memeText = document.querySelector('#meme-text');
+const preMemes = document.querySelector('#meme-templates').childNodes;
 const save = document.querySelector('#save');
-
+const water = document.querySelector('#water');
 
 inputText.addEventListener('input', () => {
   memeText.innerHTML = inputText.value;
@@ -36,6 +33,10 @@ earth.addEventListener('click', () => {
   container.style.border = '6px groove green';
 });
 
+borderDefault.addEventListener('click', () => {
+  container.style.border = '1px solid black';
+});
+
 preMemes.forEach((element) => {
   element.addEventListener('click', () => {
     memeImage.src = element.src;
@@ -44,9 +45,16 @@ preMemes.forEach((element) => {
 
 // source https://dev.to/cjuniordev/transformando-html-em-imagens-2k5e
 save.addEventListener('click', () => {
-  html2canvas(document.querySelector('#meme-image-container')).then(canvas => {
+  // eslint-disable-next-line no-undef
+  html2canvas(document.querySelector('#meme-image-container')).then((canvas) => {
     btnDownload.href = canvas.toDataURL('image/png');
-    btnDownload.download =  'minha-imagem';
+    btnDownload.download = 'minha-imagem';
     btnDownload.click();
-  })
+  });
+});
+
+localText.forEach((element) => {
+  element.addEventListener('click', () => {
+    memeText.style.top = element.value;
+  });
 });
