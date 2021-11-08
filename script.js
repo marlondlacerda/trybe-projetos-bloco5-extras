387a471f570e5e96a1e789420cd91570e8e451ca
Marlon Lacerda <marlon.desagitario@hotmail.com>
2021-11-08 19:57:32 -0300

    Color guess updated

--- script.js
+++ script.js
@@ -1,128 +1,92 @@
-// Algorítmo para criar uma cor aleatoria e retornando com um resultado
-function randomColorGenerator() {
-  // source https://css-tricks.com/snippets/javascript/random-hex-color/
-  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  return (`#${randomColor}`);
-}
+const ball = '.ball';
 
-// Escolhe uma das cores da ball para ser a resposta
-function randomSelected() {
+const answer = document.querySelector('#answer');
+const ballsContainer = document.querySelector('#balls-container');
+const colorSelected = document.querySelector('#rgb-color');
+const sumPoints = document.querySelector('#score');
+const resetGame = document.querySelector('#reset-game');
+const hardMode = document.querySelector('#hard-mode');
+const hardModeStatus = document.querySelector('#hard-status');
+
+const removeRgbText = (text) => text.replace('rgb', '');
+
+const randomColorGenerator = () => {
+  // source https://www.codegrepper.com/code-examples/javascript/generate+random+rgb+color+javascript
+  const r = () => Math.random() * 256 || 0;
+  return `rgb(${r()}, ${r()}, ${r()})`;
+};
+
+const selectAColor = () => {
+  const balls = document.querySelectorAll('.ball');
   // source https://stackoverflow.com/questions/7595557/selecting-a-random-html-element-using-javascript-only
-  const randomColorSelected = document.querySelectorAll('.ball');
-  const ballIndex = Math.floor(Math.random() * randomColorSelected.length);
-  let newtext = randomColorSelected[ballIndex].style.backgroundColor;
-  newtext = newtext.replace('rgb', '');
-  return newtext;
-}
+  const ballIndex = Math.floor(Math.random() * balls.length);
+  return removeRgbText(balls[ballIndex].style.backgroundColor);
+};
 
-// Cria dinamicamente um h4 onde vai ficar o texto rg da cor selecionada para o resultado
-function whatColor() {
-  const colorText = document.querySelector('#rgb-color-selected');
-  const idText = document.createElement('h4');
-  idText.id = 'rgb-color';
-  idText.innerText = randomSelected();
-  colorText.appendChild(idText);
-}
+const ballMaker = () => {
+  const numberOffBalls = hardModeStatus.innerText === 'ON' ? 12 : 6;
 
-// Cria as divs contendo as balls
-function ballsCreate() {
-  const coresSection = document.querySelector('#balls');
-  const cores = document.createElement('div');
-  cores.id = ('cores');
-  coresSection.appendChild(cores);
-  for (let index = 1; index < 7; index += 1) {
-    const divBalls = document.createElement('div');
-    divBalls.className = 'ball';
-    divBalls.style.backgroundColor = randomColorGenerator();
-    cores.appendChild(divBalls);
+  const balls = document.querySelectorAll(ball);
+  balls.forEach((element) => {
+    element.remove();
+  });
+
+  for (let index = 0; index < numberOffBalls; index += 1) {
+    const buttonBall = document.createElement('button');
+    buttonBall.style.backgroundColor = randomColorGenerator();
+    buttonBall.className = 'ball';
+    ballsContainer.appendChild(buttonBall);
   }
-}
+};
 
-// Função para resetar e refazer o game
-function resetRemove(resposta) {
-  document.getElementById('cores').remove();
-  document.getElementById('rgb-color').remove();
-  const newText = resposta;
-  newText.innerText = 'Escolha uma cor';
-  ballsCreate();
-  whatColor();
-}
+const inpector = (selected) => {
+  const balls = document.querySelectorAll(ball);
+  const answerSelected = document.querySelector('#rgb-color').innerText;
 
-function verify(click, resposta) {
-  let colorQuestion = document.querySelector('h4');
-  colorQuestion = `rgb${colorQuestion.innerText}`;
-  const sumPoints = document.getElementById('score');
-  const novoTexto = resposta;
+  balls.forEach((element) => {
+    const button = element;
+    button.disabled = true;
+  });
 
-  if (click.target.style.backgroundColor === colorQuestion) {
-    novoTexto.innerText = 'Acertou!';
-    sumPoints.innerText = parseInt(sumPoints.innerText, 10) + 3;
+  if (selected === answerSelected) {
+    answer.innerText = 'Acertou!';
+    const points = hardModeStatus.innerText === 'ON' ? 6 : 3;
+    sumPoints.innerText = parseInt(sumPoints.innerText, 10) + points;
   } else {
-    novoTexto.innerText = 'Errou! Tente novamente!';
+    answer.innerText = 'Errou! Tente novamente!';
     sumPoints.innerText = parseInt(sumPoints.innerText, 10) - 1;
   }
-}
+};
 
-// função onde vai ouvir o click e executar uma ação
-function result(click) {
-  const selecionado = document.querySelector('.selecionado');
-  const resposta = document.getElementById('answer');
+const newGame = () => {
+  const balls = document.querySelectorAll(ball);
+  answer.innerText = 'Escolha uma cor';
+  ballMaker();
+  colorSelected.innerText = selectAColor();
 
-  if (click.target.classList.contains('ball')) {
-    if (selecionado === null) {
-      click.target.classList.add('selecionado');
-      verify(click, resposta);
-    } else {
-      selecionado.classList.remove('selecionado');
-      click.target.classList.add('selecionado');
-      verify(click, resposta);
-    }
-  }
+  balls.forEach((element) => {
+    const button = element;
+    button.disabled = false;
+  });
+};
 
-  if (click.target.id === 'reset-game') {
-    resetRemove(resposta);
-  }
-}
-
-// Irá criar os botões do roda pé do site
-function buttonsCreate() {
-  const footerbuttons = document.querySelector('#buttons');
-
-  const answerText = document.createElement('h2');
-  const buttonDificult = document.createElement('button');
-  const buttonReset = document.createElement('button');
-
-  answerText.id = 'answer';
-  buttonReset.id = 'reset-game';
-
-  answerText.innerText = 'Escolha uma cor';
-  buttonDificult.innerText = 'Mudar dificuldade';
-  buttonReset.innerText = 'Resetar jogo/cores';
-
-  footerbuttons.appendChild(answerText);
-  footerbuttons.appendChild(buttonDificult);
-  footerbuttons.appendChild(buttonReset);
-}
-
-// Irá criar dinamicamente o texto onde ficará o placar
-function createPlacar() {
-  const main = document.querySelector('#rgb-placar');
-  const placar = document.createElement('h4');
-  placar.id = 'points';
-  placar.innerHTML = 'Placar: ';
-
-  const score = document.createElement('h4');
-  score.id = 'score';
-  score.innerText = 0;
-  main.appendChild(placar);
-  main.appendChild(score);
-}
+const changeMode = () => {
+  const status = hardModeStatus.innerText;
+  hardModeStatus.innerText = status === 'ON' ? 'OFF' : 'ON';
+  ballMaker();
+  colorSelected.innerText = selectAColor();
+};
 
 window.onload = () => {
-  ballsCreate();
-  whatColor();
-  buttonsCreate();
-  createPlacar();
+  ballMaker();
+  colorSelected.innerText = selectAColor();
 
-  document.addEventListener('click', result);
+  ballsContainer.addEventListener('click', ({ target }) => {
+    const selected = removeRgbText(target.style.backgroundColor);
+    inpector(selected);
+  });
+
+  resetGame.addEventListener('click', () => newGame());
+
+  hardMode.addEventListener('click', () => changeMode());
 };
